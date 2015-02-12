1d61bc6c373cf178a1a277b6f2f0e72b31707d9f
Ashley Ellis <anellis12@gmail.com>
2015-02-12 12:40:25 -0500

    Add ability for authors to sign up #shipit

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ ruby '2.2.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -25,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,57 @@
+class AuthorsController < ApplicationController
+  before_action :set_author, only: [:edit, :update, :destroy]
+  #before_action :authenticate_user, except: [:new, :create]
+
+  def new
+    @author = Author.new
+  end
+
+  def edit
+  end
+
+  def show
+    render :edit
+  end
+
+  def index
+    @authors= Author.all
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to authors_path, notice: 'Author was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    if @author.update(author_params)
+      redirect_to authors_path, notice: 'Author was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @author.destroy
+    redirect_to author_url, notice: 'Author was successfully destroyed.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_author
+    @author = Author.find(session[:author_id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def author_params
+    params.require(:author).permit(:name, :email, :password)
+  end
+
+  def make_session(author)
+    session[:author_id] = author.id
+  end
+end
