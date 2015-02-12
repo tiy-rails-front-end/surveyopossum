2d3f94c1dcd0dfecf11ca7266402360c129b6197
Ruben <ssrcruz@gmail.com>
2015-02-12 16:15:35 -0500

    Add submissions controller.

---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,24 @@
+class SubmissionsController < ApplicationController
+  before_action :set_submission, only: [:edit, :update, :destroy, :show]
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+    
+  end
+
+  def index
+  end
+end
