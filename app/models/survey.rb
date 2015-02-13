33c1a13eb82ebf6d57bb2316ddfbd7dded5fe59e
Ruben <ssrcruz@gmail.com>
2015-02-13 13:30:02 -0500

    Changes

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,10 +2,9 @@ class Survey < ActiveRecord::Base
   has_many :questions, :dependent => :destroy
   has_many :submissions
   belongs_to :author
-<<<<<<< HEAD
   has_many :submissions
   accepts_nested_attributes_for :questions
-=======
+
   validates :title, presence: true
   validates :description, presence: true
   accepts_nested_attributes_for :questions,
@@ -14,5 +13,5 @@ class Survey < ActiveRecord::Base
   accepts_nested_attributes_for :questions,
   :allow_destroy => true,
   :reject_if     => :all_blank
->>>>>>> 15ef07607c7c3d8646262d6e13a50d7e2d946de6
+
 end
