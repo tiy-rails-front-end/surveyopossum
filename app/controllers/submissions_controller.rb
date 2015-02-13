4c7ec46765c7c7dadbc154f78442a3ab3c8123aa
Ruben <ssrcruz@gmail.com>
2015-02-13 11:06:02 -0500

    Minor changes.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,22 +1,30 @@
 class SubmissionsController < ApplicationController
-  before_action :set_submission, only: [:edit, :update, :destroy, :show]
-  def new
-  end
+  before_action :set_submission, only: [:update, :show]
 
-  def edit
+  def new
+    @submission = Submission.new
+    question = @survey.questions.build
+    question.answers.build
   end
 
   def create
+    @submission = Submission.new(submission_params)
+
+    respond_to do |format|
+      if @submission.save
+        format.html { redirect_to @submission, notice: 'submission was successfully created.' }
+        format.json { render :show, status: :created, location: @submission }
+      else
+        format.html { render :new }
+        format.json { render json: @submission.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
   end
 
-  def destroy
-  end
-
   def show
-    
   end
 
   def index
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ActiveRecord::Base
   has_many :questions, :dependent => :destroy
   belongs_to :author
+  has_many :submissions
   accepts_nested_attributes_for :questions
 end
