728a7fe6aaf84219b60bae85e327943e1eb2d3b9
Ashley Ellis <anellis12@gmail.com>
2015-02-14 13:52:29 -0500

    Ignore option if name is blank

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :answers
   has_many :options
-  accepts_nested_attributes_for :options
+  accepts_nested_attributes_for :options,
+  :allow_destroy => true,
+  :reject_if => proc { |attributes| attributes['name'].blank? }
 end
