da10fb89a08cdccce4434fecc7bb76a644d3ec06
Ashley Ellis <anellis12@gmail.com>
2015-02-16 00:28:51 -0500

    Still working on answers saving

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,8 @@ class Submission < ActiveRecord::Base
   has_many :answers, :dependent => :destroy
   belongs_to :survey
   accepts_nested_attributes_for :answers,
-  :allow_destroy => true
+  :allow_destroy => true,
+  :reject_if     => :all_blank
 
   def build_answers
     answers.build
